af3b66eef7d58302e98ed45d1c70da757bb37c05
Julio Cesar Lima Reis <juliolimareis@gmail.com>
2023-06-19 19:08:00 -0300

    Fix: Ajuste no componente de loader.

---
+++ js/components/loading.js
@@ -0,0 +1,47 @@
+import { ref } from "../vue-3-3-4.js"
+
+export default {
+  setup() {},
+  template: `
+    <div>
+      <div class="loader-container">
+        <div class="card-box">
+          <div class="card-header"></div>
+          <div class="card-body mt-2"></div>
+          <div class="card-desc mt-1"></div>
+        </div>
+        <div class="card-box">
+          <div class="card-header"></div>
+          <div class="card-body mt-2"></div>
+          <div class="card-desc mt-1"></div>
+        </div>
+        <div class="card-box">
+          <div class="card-header"></div>
+          <div class="card-body mt-2"></div>
+          <div class="card-desc mt-1"></div>
+        </div>
+        <div class="card-box">
+          <div class="card-header"></div>
+          <div class="card-body mt-2"></div>
+          <div class="card-desc mt-1"></div>
+        </div>
+        <div class="card-box">
+          <div class="card-header"></div>
+          <div class="card-body mt-2"></div>
+          <div class="card-desc mt-1"></div>
+        </div>
+        <div class="card-box">
+          <div class="card-header"></div>
+          <div class="card-body mt-2"></div>
+          <div class="card-desc mt-1"></div>
+        </div>
+        <div class="card-box">
+          <div class="card-header"></div>
+          <div class="card-body mt-2"></div>
+          <div class="card-desc mt-1"></div>
+        </div>
+      </div>
+      <p class="mt-5">Looking for letters ...</p>
+    </div>
+    `
+}
