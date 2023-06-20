9d2f16144da05ba72594e20933814362b741971e
Julio Cesar Lima Reis <juliolimareis@gmail.com>
2023-06-20 02:05:54 -0300

    Fix: Ajuste na responsividade

--- js/tailwind.js
+++ js/tailwind.js
@@ -1,5 +1,12 @@
 tailwind.config = {
   theme: {
+    screens: {
+      'mb-360': '360px',
+      'mb-412': '412px',
+      'mb-384': '384px',
+      'mb-375': '375px',
+      'mb-800': '800px',
+    },
     extend: {
       colors: {
         clifford: '#da373d',
